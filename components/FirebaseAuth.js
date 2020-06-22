2a43f801f06f14ba4671ec67a6e01f7b8e560046
Peter Arnold <peter@deepsixdesign.com>
2020-06-22 19:23:39 -0400

    asd

--- components/FirebaseAuth.js
+++ components/FirebaseAuth.js
@@ -1,52 +1,53 @@
 /* globals window */
-import { useEffect, useState } from 'react'
-import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth'
-import firebase from 'firebase/app'
-import 'firebase/auth'
-import cookie from 'js-cookie'
-import initFirebase from '../utils/auth/initFirebase'
+import { useEffect, useState } from "react";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+import firebase from "firebase/app";
+import "firebase/auth";
+import cookie from "js-cookie";
+import initFirebase from "../utils/auth/initFirebase";
 
 // Init the Firebase app.
-initFirebase()
+initFirebase();
 
 const firebaseAuthConfig = {
-  signInFlow: 'popup',
+  signInFlow: "popup",
   // Auth providers
   // https://github.com/firebase/firebaseui-web#configure-oauth-providers
   signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
     {
       provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
       requireDisplayName: false,
     },
   ],
-  signInSuccessUrl: '/',
-  credentialHelper: 'none',
+  signInSuccessUrl: "/",
+  credentialHelper: "none",
   callbacks: {
     signInSuccessWithAuthResult: async ({ user }, redirectUrl) => {
       // xa is the access token, which can be retrieved through
       // firebase.auth().currentUser.getIdToken()
-      const { uid, email, xa } = user
+      const { uid, email, xa } = user;
       const userData = {
         id: uid,
         email,
         token: xa,
-      }
-      cookie.set('auth', userData, {
+      };
+      cookie.set("auth", userData, {
         expires: 1,
-      })
+      });
     },
   },
-}
+};
 
 const FirebaseAuth = () => {
   // Do not SSR FirebaseUI, because it is not supported.
   // https://github.com/firebase/firebaseui-web/issues/213
-  const [renderAuth, setRenderAuth] = useState(false)
+  const [renderAuth, setRenderAuth] = useState(false);
   useEffect(() => {
-    if (typeof window !== 'undefined') {
-      setRenderAuth(true)
+    if (typeof window !== "undefined") {
+      setRenderAuth(true);
     }
-  }, [])
+  }, []);
   return (
     <div>
       {renderAuth ? (
@@ -56,7 +57,7 @@ const FirebaseAuth = () => {
         />
       ) : null}
     </div>
-  )
-}
+  );
+};
 
-export default FirebaseAuth
+export default FirebaseAuth;
