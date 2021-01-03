5a42e67916ec77ae5fa31ba999245a405d9346d0
Peter Arnold <peter@entroprise.com>
2021-01-02 22:00:45 -0500

    asd

--- config/index.ts
+++ config/index.ts
@@ -2,4 +2,5 @@
  * Configuration
  */
 export const SERVER = "https://tolerant-sparrow-62.hasura.app/v1/graphql";
-export const WEB_SOCKET_LINK = "ws://tolerant-sparrow-62.hasura.app/v1/graphql";
+export const WEB_SOCKET_LINK =
+  "wss://tolerant-sparrow-62.hasura.app/v1/graphql";
