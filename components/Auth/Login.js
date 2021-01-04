6ad6a2520f2135afdd190278071b24fa1674882a
Peter Arnold <peter@entroprise.com>
2021-01-04 18:16:26 -0500

    asd

--- components/Auth/Login.js
+++ components/Auth/Login.js
@@ -1,19 +1,35 @@
 import Router from "next/router";
-import { Button, Typography } from "@material-ui/core";
+import {
+  Typography,
+  Button,
+  Box,
+  Card,
+  CardHeader,
+  CardContent,
+} from "@material-ui/core";
 
 import Link from "../Link";
 
 const Login = () => {
   return (
-    <div>
-      <Typography variant="h3">Entroprise</Typography>
-      <Typography>Please login to continue</Typography>
-      <Link href="/api/login" passHref>
-        <Button variant="contained" color="secondary">
-          Log In
-        </Button>
-      </Link>
-    </div>
+    <Box mt={2} mb={1}>
+      <Card variant="outlined">
+        <CardHeader
+          title="Entroprise"
+          subheader="Merging the finite with the infinite."
+        />
+        <CardContent>
+          <Typography>Please login to continue</Typography>
+          <Box mt={2} mb={1}>
+            <Link href="/api/login" passHref>
+              <Button variant="outlined" color="primary">
+                Log In
+              </Button>
+            </Link>
+          </Box>
+        </CardContent>
+      </Card>
+    </Box>
   );
 };
 
