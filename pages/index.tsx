930b722b58adf002dc2dc75adcd4ce68ade406e0
Peter Arnold <peter@entroprise.com>
2021-01-15 19:11:35 -0500

    logo

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,41 +6,52 @@ import {
   CardHeader,
   CardContent,
 } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
 import Logo from "../components/Logo";
 
-const AboutPage = () => (
-  <Layout title="About | Entroprise">
-    <Box my={3}>
-      <Logo />
-    </Box>
-    <Box mt={2} mb={1}>
-      <Card variant="outlined">
-        <CardHeader
-          title="About"
-          subheader="Merging the finite with the infinite."
-        />
-        <CardContent>
-          <Typography>
-            In mainstream theories of natural language syntax, every
-            syntactically-valid utterance can be extended to produce a new,
-            longer one, because of recursion. If this process can be continued
-            indefinitely, then there is no upper bound on the length of a
-            well-formed utterance and the number of unique well-formed strings
-            of any language is countably infinite. However, the books in the
-            Library of Babel are of bounded--**/ length ("each book is of four
-            hundred and ten pages; each page, of forty lines, each line, of some
-            eighty letters"), so the Library can only contain a finite number of
-            distinct strings, and thus cannot contain all possible well-formed
-            utterances. Borges' narrator notes this fact, but believes that the
-            Library is nevertheless infinite; he speculates that it repeats
-            itself periodically, giving an eventual "order" to the "disorder" of
-            the seemingly-random arrangement of books.
-          </Typography>
-        </CardContent>
-      </Card>
-    </Box>
-  </Layout>
-);
+const useStyles = makeStyles((theme) => ({
+  box: {
+    paddingLeft: theme.spacing(2),
+    paddingRight: theme.spacing(2),
+  },
+}));
+
+const AboutPage = () => {
+  const classes = useStyles();
+  return (
+    <Layout title="About | Entroprise">
+      <Box className={classes.box}>
+        <Logo />
+      </Box>
+      <Box>
+        <Card variant="outlined">
+          <CardHeader
+            title="About"
+            subheader="Merging the finite with the infinite."
+          />
+          <CardContent>
+            <Typography>
+              In mainstream theories of natural language syntax, every
+              syntactically-valid utterance can be extended to produce a new,
+              longer one, because of recursion. If this process can be continued
+              indefinitely, then there is no upper bound on the length of a
+              well-formed utterance and the number of unique well-formed strings
+              of any language is countably infinite. However, the books in the
+              Library of Babel are of bounded--**/ length ("each book is of four
+              hundred and ten pages; each page, of forty lines, each line, of
+              some eighty letters"), so the Library can only contain a finite
+              number of distinct strings, and thus cannot contain all possible
+              well-formed utterances. Borges' narrator notes this fact, but
+              believes that the Library is nevertheless infinite; he speculates
+              that it repeats itself periodically, giving an eventual "order" to
+              the "disorder" of the seemingly-random arrangement of books.
+            </Typography>
+          </CardContent>
+        </Card>
+      </Box>
+    </Layout>
+  );
+};
 
 export default AboutPage;
