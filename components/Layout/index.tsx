db651aa2136d6c95cccf3bad0cc3a503aecfa664
Peter Arnold <peter@entroprise.com>
2021-01-19 10:03:27 -0500

    pwa

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -14,7 +14,29 @@ const Layout = ({ children, title = "Entroprise" }: Props) => (
     <Head>
       <title>{title}</title>
       <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta
+        name="viewport"
+        content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+      />
+      <meta name="description" content="Description" />
+      <meta name="keywords" content="Keywords" />
+      <title>{title}</title>
+
+      <link rel="manifest" href="/manifest.json" />
+      <link
+        href="/favicon-16x16.png"
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+      />
+      <link
+        href="/favicon-32x32.png"
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+      />
+      <link rel="apple-touch-icon" href="/apple-icon-180x180.png"></link>
+      <meta name="theme-color" content="#556cd6" />
     </Head>
     <Core title={title}>{children}</Core>
     <Footer />
